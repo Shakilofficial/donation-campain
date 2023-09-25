3801ab110a91a2dad17e6be2544319e12846e537
Md Shakil Hossain <mrshakilhossainofficial@gmail.com>
2023-09-26 04:38:21 +0600

    home page complited

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import Donation from "../../components/Donation/Donation";
 import Banner from "../../components/Header/Banner/Banner";
 
 const Home = () => {
+
+    const [donations, setDonations] = useState([]);
+
+    useEffect(() => {
+        fetch('/public/donation.json')
+        .then(res => res.json())
+        .then(data => setDonations(data));
+    
+    },[])
+
     return (
         <div>
             <div className="">
                 <Banner></Banner>
+            </div>
+            <div className="grid px-4 md:px-0 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-16 gap-4">
+                {
+                    donations.map(donation => <Donation key={donation.category} donation={donation} ></Donation>  )
+                }
             </div>
         </div>
     );
--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div>
-      <div className="bg-[url('https://i.ibb.co/FwTHwq2/people-meeting-community-center.jpg')] bg-cover bg-center h-[600px] text-black bg-opacity-10 bg-blend-overlay">
+      <div className="bg-[url('https://i.ibb.co/FwTHwq2/people-meeting-community-center.jpg')] bg-cover bg-center h-[600px] text-black">
         <div className="bg-white bg-opacity-90 h-full space-y-4 flex flex-col justify-center items-center">
           <h1 className="text-5xl font-bold mb-2">
             I Grow By Helping People In Need
@@ -9,7 +9,7 @@ const Banner = () => {
           <div className="flex">
             <input
               type="text"
-              placeholder="Search..."
+              placeholder="Search here...."
               className="px-2 py-2 border-2 rounded-md bg-white text-gray-800"
             />
             <button className="px-6 py-2 rounded-md bg-red-500 text-white">Search</button>
